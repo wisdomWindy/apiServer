92114634d095f01e580b6a8f30d844df0a21f53d
wisdomwindy <1659140773@qq.com>
2022-08-15 21:27:11 +0800

    添加文件

--- router/upload.js
+++ router/upload.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const uploadHandler = require('../router_handler/upload');
 
 router.post('/normal', uploadHandler.uploadNormal);
-
+router.post('/uploadBase64', uploadHandler.uploadBase64);
 router.post('/big', uploadHandler.uploadBig);
 
 module.exports = router;
--- router_handler/upload.js
+++ router_handler/upload.js
@@ -1,22 +1,46 @@
 const path = require('path');
 const formidable = require('formidable');
+const sparkmd5 = require('spark-md5');
+const fs = require('fs');
 
-function uploadNormal(req,res){
-  let form = new formidable.IncomingForm({multiples:true, uploadDir: path.join(__dirname, '..','uploads'), keepExtensions:true});
-  form.parse(req,function(err, fields, files){
-    if(err){
+function uploadNormal(req, res) {
+  let form = new formidable.IncomingForm({ multiples: true, uploadDir: path.join(__dirname, '..', 'uploads'), keepExtensions: true });
+  form.parse(req, function (err, fields, files) {
+    if (err) {
       res.send(err);
       return;
     }
     res.send({ code: 200, errMessage: '上传成功' });
   });
-  
+
+}
+function uploadBase64(req, res) {
+  console.log(req.body)
+  let fileData = req.body,
+    file = fileData.file,
+    filename = fileData.filename,
+    spark = new sparkmd5.ArrayBuffer(),
+    suffix = /\.([0-9a-zA-Z]+)$/.exec(filename)[1],
+    path;
+  file = decodeURIComponent(file);
+  file = file.replace(/^(data:image\/\w+;base64,)/, '');
+  file = Buffer.from(file, 'base64');
+  spark.append(file);
+  path = `${path.join(__dirname, '..', 'upload')}/${spark.end()}.${suffix}`;
+  fs.writeFile(path, file, (err) => {
+    if (err) {
+      res.cc(400, err);
+      return;
+    }
+    res.send({ code: 200, errMessage: '上传成功' });
+  });
 }
 
-function uploadBig(req,res){
+function uploadBig(req, res) {
 
 }
 module.exports = {
   uploadNormal,
-  uploadBig
+  uploadBig,
+  uploadBase64
 }
