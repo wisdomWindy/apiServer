18c95ae3141e8a95057931b796ebc61b68e9c738
wisdomwindy <1659140773@qq.com>
2022-08-16 20:21:30 +0800

    添加文件

--- router_handler/upload.js
+++ router_handler/upload.js
@@ -37,7 +37,73 @@ function uploadBase64(req, res) {
 }
 
 function uploadBig(req, res) {
-
+  let {
+    HASH,
+    count
+  } = req.body;
+  let uploadDir = path.join(__dirname, '..', 'uploads');
+  merge(HASH,count).then(res => {
+    let {
+      filename,
+      path
+    } = res;
+    res.send({
+      code:0,
+      errMessage:'merge success',
+      originalFilename:filename,
+      servePath:path.replace(__dirname,HOSTNAME)
+    });
+  }).catch(err=>{
+    res.cc(400,err);
+  });
+}
+function uploadAlready(req,res){
+  let {
+    HASH
+  } = req.body;
+  let uploadDir = path.join(__dirname, '..', 'uploads');
+  try{
+    let path = `${uploadDir}/${HASH}`,
+      fileList = [];
+    fileList = fs.readFileSync(path);
+    fileList = fileList.sort((a, b) => {
+      let reg = /_(\d+)/;
+      return reg.exec(a)[1] - reg.exec(b)[1];
+    });
+    res.send({
+      code: 200,
+      errMessage: 'success',
+      fileList: fileList
+    });
+  }catch(err){
+    res.cc(400, err);
+  };
+}
+function merge(HASH,count){
+  return new Promise((resolve,reject) => {
+    let path = `${uploadDir}/${HASH}`,
+    fileList =[],
+    suffix,
+    isExists;
+    isExists = exists(path);
+    if(!isExists){
+      reject('HASH path is not found');
+      return;
+    }
+    fileList.sort((a,b)=>{
+      let reg = /_(\d+)/;
+      return reg.exec(a)[1] - reg.exec(b)[1];
+    }).forEach(item => {
+      !isuffix ? suffix = /\.([0-9a-zA-Z]+)$/.exec(item)[1] : null;
+      fs.appendFileSync(`${uploadDir}/${HASH}.${suffix}`, fs.readFileSync(`${path}/${item}`));
+      fs.unlinkSync(`${path}/${item}`);
+    });
+    fs.rmdirSync(path);
+    resolve({
+      path:`${uploadDir}/${HASH}.${suffix}`,
+      filename:`${HASH}.${suffix}`
+    });
+  });
 }
 module.exports = {
   uploadNormal,
